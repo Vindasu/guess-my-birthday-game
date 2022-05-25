b0a8b34b0df0c9e240636c02e32c40ed58bdb9f7
Alex <alexleeadams@aol.com>
2022-05-25 15:39:58 -0500

    updated to remove redundancy

--- gameloop.py
+++ gameloop.py
@@ -4,9 +4,6 @@ name = input("Hi! What is your name? ")
 
 # Guess 1
 
-month_number = randint(1, 12)
-year_number = randint(1924, 2004)
-
 guesses = range(1, 6)
 for x in guesses:
     month_number = randint(1, 12)
